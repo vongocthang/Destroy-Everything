c9732a269868f6084608d61db2b96b75db86196c
Võ Ngọc Thắng <vongocthang160199@gmail.com>
2021-09-23 20:31:44 +0700

    no Note

---
+++ Destroy Everything/Assets/Scripts/test.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start");
+        InvokeRepeating("Delay", 5, 1);
+    }
+
+    void Delay()
+    {
+        Debug.Log("OK");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
